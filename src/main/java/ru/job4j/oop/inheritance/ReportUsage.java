87f6bce1fb067215c446bcb6128403c394ee6f86
Chernovilya10 <i.chernov93@yandex.ru>
2020-05-19 20:56:42 +0300

    4. Переопределение[#279106]

---
+++ src/main/java/ru/job4j/oop/inheritance/ReportUsage.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop.inheritance;
+
+public class ReportUsage {
+    public static void main(String[] args) {
+        TextReport report = new TextReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
+
+        JSONReport jsonReport = new JSONReport();
+        String text1 = jsonReport.generate("Report's name", "Report's body");
+        System.out.println(text1);
+    }
+}
