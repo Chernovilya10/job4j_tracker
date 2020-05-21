9d94fb42184b455bf3ca7dd4e4fa02c08bd55a0c
Chernovilya10 <i.chernov93@yandex.ru>
2020-05-22 00:26:29 +0300

    5. Tracker - хранилище[396#279118]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,26 +1,81 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
     private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
+    private int position = 0;
 
+    /**
+     * add - Метод добавляет в массив this.items модель с новым случайно сгенерированным уник. ключом.
+     * @param item - передаваемый параметр, в данном случае модель.
+     * @return - возвращаем модель уже с новым уник. ключом.
+     */
     public Item add(Item item) {
         item.setId(generateId());
-        items[size++] = item;
+        items[position++] = item;
         return item;
     }
 
+    /**
+     * Для генерации уник. ключа необход. исп-ть кл. Random, у кот. есть метод nextLong() - он возвращает произвольное число
+     * Метод System.currentTimeMillis() возвращает число типа Long - кол-во миллисеккунд прошло с 1 янв 1970 года
+     * Для преобразования переменной в тип String исп-ем метод String.valueOf
+     * @return - возвращает стринговый уник. ключ.
+     */
     private String generateId() {
-        return String.valueOf(ids++);
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    /**
+     * findAll - метод используется для возврата копии массива this.items без элементов null
+     * Item[] itemsWithOutNull = new Item[items.length]; - создали новый массив, у которого будет такая же длина, как и у this.items
+     * Далее перебираем не null эл-ты массива items и записываем по очереди в новый массив.
+     * Arrays.copyOf(itemsWithOutNull, size) - обрезаем новый массив до того количества эл-ов, которые были переданы, чтобы избавиться от null элементов.
+     * @return - возвращает копию массива this.itemsбез элементов null
+     */
+    public Item[] findAll() {
+        Item[] itemsWithOutNull = new Item[position];
+        int size = 0;
+        for (int i = 0; i < position; i++) {    //использую position, т.к. макс кол-во эл-ов в массиве items будет равно position
+            Item it = items[i];
+            if (it != null) {
+                itemsWithOutNull[size] = it;
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemsWithOutNull, size);
+    }
+
+    /**
+     * findName - метод используется для возврата копии массива this.items c совпадающим заданным именем.
+     * Item[] itemsWithEqualsName = new Item[items.length]; - создали новый массив, у которого будет такая же длина, как и у this.items
+     * Далее перебираем эл-ты массива items с одинаковым заданным именем key и записываем по очереди в новый массив.
+     * Arrays.copyOf(itemsWithEqualsName, size) - обрезаем новый массив до того количества эл-ов, которые были переданы, чтобы избавиться от null элементов.
+     * @return - возвращает копию массива this.items c заданным именем.
+     */
+
+    public Item[] findName(String key) {
+        Item[] itemsWithEqualsName = new Item[position];
+        int size = 0;
+        for (int i = 0; i < position; i++) {    //использую position, т.к. макс кол-во эл-ов в массиве items будет равно position
+            Item it = items[i];
+            if (it.getName().equals(key)) {
+                itemsWithEqualsName[size] = it;
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemsWithEqualsName, size);
     }
 
     public Item findById(String id) {
         Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId().equals(id)) {
-                rsl = item;
+        for (int i = 0; i < position; i++) {        //использую position, т.к. макс кол-во эл-ов в массиве items будет равно position
+            Item it = items[i];
+            if (it.getId().equals(id)) {
+                rsl = it;
                 break;
             }
         }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -13,4 +13,37 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+    @Test
+    public void whenAddNewArrayOfItemThenTrackerHasName() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test");
+        Item item2 = new Item("test");
+        Item item3 = new Item("t");
+        Item item4 = new Item("test");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item[] result = tracker.findName("test");
+        Item[] expect = {item1, item2, item4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenAddNewArrayOfItemThenTrackerHasAllWithOutNull() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test");
+        Item item2 = new Item("tst");
+        Item item3 = new Item("tt");
+        Item item4 = new Item("tet");
+        Item item5 = new Item("tet");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        tracker.add(item5);
+        Item[] result = tracker.findAll();
+        Item[] expect = {item1, item2, item3, item4, item5};
+        assertThat(result, is(expect));
+    }
 }
