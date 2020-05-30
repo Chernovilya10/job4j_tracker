41ee13d6197dd8ddc4f807aa70ae577c0d34e695
Chernovilya10 <i.chernov93@yandex.ru>
2020-05-30 16:12:41 +0300

    4.2. Статические методы.[181779#279057]

--- src/main/java/ru/job4j/poly/StartUI.java
+++ src/main/java/ru/job4j/poly/StartUI.java
@@ -5,77 +5,97 @@ import ru.job4j.tracker.Input;
 import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
 
-
 /**
  * Данный класс позволяет нам работать с классом Scanner, через интрефейс Input, для выполнения методов класса Tracker
  * с помощью обращения пользователя через консоль.
  */
 public class StartUI {
 
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("==== Create a new Item ====");
+        String name = input.askString("Enter Name");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void showAllItems(Tracker tracker) {
+        System.out.println("==== Show all Items without null elements ====");
+        Item[] findAll = tracker.findAll();
+        if (findAll.length == 0) {
+            System.out.println("Please crate new Item!");        //Если не добавили модель
+        } else {
+            for (int i = 0; i < findAll.length; i++) {
+                System.out.println("Name: " + findAll[i].getName() + "; " + "Id: " + findAll[i].getId());
+            }
+        }
+    }
+
+    public static void editItem(Input input, Tracker tracker) {
+        System.out.println("==== Edit item ====");
+        String id = input.askString("Please enter Id");
+        String name = input.askString("Please enter new Name of the item");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Error");
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        System.out.println("==== Delete item ====");
+        String id = input.askString("Please enter Id");
+        if (tracker.delete(id)) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Error");
+        }
+    }
+
+    public static void findItemById(Input input, Tracker tracker) {
+        System.out.println("==== Find item by Id ====");
+        String id = input.askString("Please enter Id");
+        Item findById = tracker.findById(id);
+        System.out.println("Name: " + findById.getName() + "; " + "Id: " + findById.getId());
+    }
+
+    public static void findItemsByName(Input input, Tracker tracker) {
+        System.out.println("==== Find items by name ====");
+        String name = input.askString("Please enter Name");
+        Item[] findName = tracker.findName(name);
+        if (findName.length == 0) {
+            System.out.println("Error. This name not found or didn't create item.");        //Если не добавили модель или нет такого имени
+        } else {
+            for (int i = 0; i < findName.length; i++) {
+                System.out.println("Name: " + findName[i].getName() + "; " + "Id: " + findName[i].getId());
+            }
+        }
+    }
+
+    public static boolean exit() {
+        System.out.println("Bye bye.");
+        return false;
+    }
+
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
-//            System.out.println("Select:");
             int select = input.askInt("Select:");
             if (select == 0) {
-                System.out.println("==== Create a new Item ====");
-//                System.out.println("Enter Name");
-                String name = input.askString("Enter Name");
-                Item item = new Item(name);
-                tracker.add(item);
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
-                System.out.println("==== Show all Items without null elements ====");
-                Item[] findAll = tracker.findAll();
-                if (findAll.length == 0) {
-                    System.out.println("Please crate new Item!");        //Если не добавили модель
-                } else {
-                    for (int i = 0; i < findAll.length; i++) {
-                        System.out.println("Name: " + findAll[i].getName() + "; " + "Id: " + findAll[i].getId());
-                    }
-                }
+                StartUI.showAllItems(tracker);
             } else if (select == 2) {
-                System.out.println("==== Edit item ====");
-//                System.out.println("Please enter Id");
-                String id = input.askString("Please enter Id");
-//                System.out.println("Please enter new Name of the item");
-                String name = input.askString("Please enter new Name of the item");
-                Item item = new Item(name);
-                if (tracker.replace(id, item)) {
-                    System.out.println("Success");
-                } else {
-                    System.out.println("Error");
-                }
+                StartUI.editItem(input, tracker);
             } else if (select == 3) {
-                System.out.println("==== Delete item ====");
-//                System.out.println("Please enter Id");
-                String id = input.askString("Please enter Id");
-                if (tracker.delete(id)) {
-                    System.out.println("Success");
-                } else {
-                    System.out.println("Error");
-                }
+                StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
-                System.out.println("==== Find item by Id ====");
-//                System.out.println("Please enter Id");
-                String id = input.askString("Please enter Id");
-                Item findById = tracker.findById(id);
-                System.out.println("Name: " + findById.getName() + "; " + "Id: " + findById.getId());
+                StartUI.findItemById(input, tracker);
             } else if (select == 5) {
-                System.out.println("==== Find items by name ====");
-//                System.out.println("Please enter Name");
-                String name = input.askString("Please enter Name");
-                Item[] findName = tracker.findName(name);
-                if (findName.length == 0) {
-                    System.out.println("Error. This name not found or didn't create item.");        //Если не добавили модель или нет такого имени
-                } else {
-                    for (int i = 0; i < findName.length; i++) {
-                        System.out.println("Name: " + findName[i].getName() + "; " + "Id: " + findName[i].getId());
-                    }
-                }
+                StartUI.findItemsByName(input, tracker);
             } else if (select == 6) {
-                System.out.println("Bye bye.");
-                run = false;
+                run = StartUI.exit();
             } else if (select < 0 && select > 6) {
                 System.out.println("Repeat again.");
             }
