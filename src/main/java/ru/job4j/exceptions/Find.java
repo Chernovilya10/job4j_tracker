dedc64ccd3a344237f8fbfdbe326d3c79343b13b
Chernovilya10 <i.chernov93@yandex.ru>
2020-06-15 22:46:34 +0300

    3. Замена if-else-throw на if-throw[49895#279067]

--- src/main/java/ru/job4j/exceptions/Find.java
+++ src/main/java/ru/job4j/exceptions/Find.java
@@ -5,15 +5,12 @@ public class Find {
         if (index < 0 || index >= data.length) {
             throw new IllegalArgumentException("Index out of bound");
         }
-        if (data[index] == null) {
-            throw new NullPointerException("Element of the array not initialized");
-        }
             return data[index];
     }
 
     public static void main(String[] args) {
-//        String[] data = {"one", "two", "three"};
-        String[] data = new String[3];
+        String[] data = {"one", "two", "three"};
+//        String[] data = new String[3];
         String rsl = Find.get(data, 2);
         System.out.println(rsl);
     }
