2d2a114722081ee0e1623549a791c25643f0611f
chernovilya10 <i.chernov93@yandex.ru>
2020-11-10 00:27:40 +0300

    4. Сортировка номера [#279024]

--- src/main/java/ru/job4j/collections/FirstNumberSort.java
+++ src/main/java/ru/job4j/collections/FirstNumberSort.java
@@ -5,41 +5,56 @@ import java.util.Comparator;
 public class FirstNumberSort implements Comparator<String> {
     /**
      * Метод по сортировке элементов по номеру, если в коллекцию/массив добавляется элемент заголовка в формате "номер. наименование"
-     * @param first - первый элемент массива
+     *
+     * @param first  - первый элемент массива
      * @param second - второй-=-
      * @return
      */
     @Override
     public int compare(String first, String second) {
-        String firstStringNumber = "";
-        int firstNumber = 0;
-        for (int i = 0; i < first.length(); i++) {
-            char symbol = first.charAt(i);
-            if (symbol == '.') {
-                break;
-            }
-            firstStringNumber = firstStringNumber + symbol;
-            try {
-                firstNumber = Integer.parseInt(firstStringNumber);
-            } catch (Exception e) {
-                System.err.println("Указано неверное число, напиши в формате: " + "число.наименование");
-            }
-        }
+//        String firstStringNumber = "";
+//        int firstNumber = 0;
+//        for (int i = 0; i < first.length(); i++) {
+//            char symbol = first.charAt(i);
+//            if (symbol == '.') {
+//                break;
+//            }
+//            firstStringNumber = firstStringNumber + symbol;
+//            try {
+//                firstNumber = Integer.parseInt(firstStringNumber);
+//            } catch (Exception e) {
+//                System.err.println("Указано неверное число, напиши в формате: " + "число.наименование");
+//            }
+//        }
+//
+//        String secondStringNumber = "";
+//        int secondNumber = 0;
+//        for (int i = 0; i < second.length(); i++) {
+//            char symbol = second.charAt(i);
+//            if (symbol == '.') {
+//                break;
+//            }
+//            secondStringNumber = secondStringNumber + symbol;
+//            try {
+//                secondNumber = Integer.parseInt(secondStringNumber);
+//            } catch (Exception e) {
+//                System.err.println("Указано неверное число, напиши в формате: " + "число.наименование");
+//            }
+//        }
+//        return Integer.compare(firstNumber, secondNumber);
+//    }
 
-        String secondStringNumber = "";
+        String[] firstArray = first.split("\\.");
+        String[] secondArray = second.split("\\.");
+        int firstNumber = 0;
         int secondNumber = 0;
-        for (int i = 0; i < second.length(); i++) {
-            char symbol = second.charAt(i);
-            if (symbol == '.') {
+        for (int i = 0; i < Math.min(firstArray.length, secondArray.length); i++) {
+            firstNumber = Integer.parseInt(firstArray[i]);
+            secondNumber = Integer.parseInt(secondArray[i]);
+            if (firstNumber != secondNumber) {
                 break;
             }
-            secondStringNumber = secondStringNumber + symbol;
-            try {
-                secondNumber = Integer.parseInt(secondStringNumber);
-            } catch (Exception e) {
-                System.err.println("Указано неверное число, напиши в формате: " + "число.наименование");
-            }
         }
-        return Integer.compare(firstNumber, secondNumber);
+            return Integer.compare(firstNumber, secondNumber);
     }
 }
